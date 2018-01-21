8b2e761f891c8e358eba20bdf36b9cd9a80a5244
zann1x <zannix@freenet.de>
2018-01-21 17:11:56 +0100

    Added keyboard switch key, even more faces and a bug

--- app/src/main/java/com/zann1x/asciiboard/MainInputService.java
+++ app/src/main/java/com/zann1x/asciiboard/MainInputService.java
@@ -1,29 +1,34 @@
 package com.zann1x.asciiboard;
 
+import android.content.Context;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+import android.renderscript.ScriptGroup;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 public class MainInputService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private InputMethodManager inputMethodManager;
 
     private LinearLayout mainBoard;
     private ScrollView scrollView;
     private RecyclerView asciiBoardView;
     private KeyboardView keyboardView;
 
-    private AsciiFaceData asciiFaceData = new AsciiFaceData();
-
     @Override
     public void onCreate() {
         super.onCreate();
+        inputMethodManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
     }
 
     @Override
@@ -34,7 +39,7 @@ public class MainInputService extends InputMethodService implements KeyboardView
         asciiBoardView.setHasFixedSize(true);
         asciiBoardView.setLayoutManager(new LinearLayoutManager(this));
         asciiBoardView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-        asciiBoardView.setAdapter(new AsciiFaceAdapter(this, asciiFaceData.asciiFaces));
+        asciiBoardView.setAdapter(new AsciiFaceAdapter(this, AsciiFaceData.asciiFaces));
 
         scrollView = mainBoard.findViewById(R.id.scrollview);
         scrollView.addView(asciiBoardView);
@@ -69,9 +74,16 @@ public class MainInputService extends InputMethodService implements KeyboardView
         InputConnection inputConnection = getCurrentInputConnection();
 
         String text = null;
-        if (0 <= primaryCode && primaryCode <= asciiFaceData.asciiFaces.size())
-            text = asciiFaceData.asciiFaces.get(primaryCode);
+        if (0 <= primaryCode && primaryCode < AsciiFaceData.asciiFaces.size())
+            text = AsciiFaceData.asciiFaces.get(primaryCode);
 
+        /*
+        Bug: when AsciiFaceData.asciiFaces gets bigger in the future, the key codes of the
+                space and the keyboard switch key interfere with the values that are tried
+                to retrieved from the asciiFaces list
+                 --> keyboard switch instead of ascii face
+                 --> ascii face instead of space
+         */
         switch(primaryCode) {
             case Keyboard.KEYCODE_DONE:
                 inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
@@ -79,7 +91,8 @@ public class MainInputService extends InputMethodService implements KeyboardView
             case Keyboard.KEYCODE_DELETE:
                 inputConnection.deleteSurroundingText(1, 0);
                 break;
-            case 42: // magical button in the last row of the keyboard that does nothing
+            case 42: // switch to the next input method
+                inputMethodManager.switchToNextInputMethod(getWindow().getWindow().getAttributes().token, false);
                 break;
             default:
                 char code = (char) primaryCode;
